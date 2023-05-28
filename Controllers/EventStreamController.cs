a57b7beda6ff06c439e23e14a3e963860973ddc4
hafei <zhangmengcoding@gmail.com>
2023-05-28 23:37:35 +0800

    添加项目文件。

---
+++ Controllers/EventStreamController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StreamingAPI.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventStreamController : Controller
+    {
+        //this method works fine
+        [HttpGet]
+        public async Task EventStream()
+        {
+            var response = Response;
+            response.Headers.Add("Cache-Control", "no-cache");
+            response.Headers.Add("Content-Type", "text/event-stream");
+
+            for (var i = 0; i<10 ; ++i)
+            {
+                await response.WriteAsync($"data: {i}\n\n");
+                await response.Body.FlushAsync();
+
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.UseCors("AllowAllOrigins");
+app.MapControllers();
+// Enable static file middleware
+app.UseStaticFiles();
+app.Run();
